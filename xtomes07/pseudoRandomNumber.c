72bd60e7fb12d839b55f533b7f8aa25f7d1b1761
xtomes07 <78798083+xtomes07@users.noreply.github.com>
2022-12-07 12:59:05 +0100

    Add files via upload

--- xtomes07/pseudoRandomNumber.c
+++ xtomes07/pseudoRandomNumber.c
@@ -3,16 +3,18 @@
 #include "pseudoRandomNumber.h"
 
 #define LENGTH 2
+#define	FLOAT2FIXED(x)	((x) >= 0 ? ((Int16)((x)*32768+0.5)) : ((Int16)((x)*32768-0.5)))
 
 /*** constants and variables ***/
 
 Int32 a = 8145821;
 Int32 b = 3545443;
-Long accu;
+Int32 accu;
 
 static Int16 sample[LENGTH] = {-16549, 0};
 static int read = 0;
 static int write = LENGTH/2;
+float sub;
 
 
 /*** functions ***/
@@ -20,22 +22,14 @@ static int write = LENGTH/2;
 Int32 getRandomNumber (){
 	Int32 m = pow(2,16);
 
-/*
 	accu = _smpyhl(a, sample[read]);
 	accu = _sadd(accu,b);
-*/
 
 /*
-	accu = (a * sample[read]) << 1;
-	accu = accu + b;
-	accu = accu % m;
-*/
-
 	accu = (a * sample[read]);
-	accu = accu << 1;
 	accu = accu + b;
+*/
 	accu = accu % m;
-
 
 	sample[write] = accu;
 
